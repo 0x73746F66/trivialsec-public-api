13d08275735f184465128f86e61723420335a6a3
Christopher Langton <chris.langton@cmd.com.au>
2021-01-24 01:59:59 +1100

    using bash and source .env

--- test_hmac.sh
+++ test_hmac.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -x
 digest=$1 # sha256 sha512 sha3-256 sha3-384 sha3-512 blake2b512
@@ -9,28 +9,29 @@ json_data=$4
 # domain_url=https://api.trivialsec.com
 domain_url=http://localhost:8080
 # -H "Referer: ${base_path}${path_uri}"
-api_key=${LOCAL_API_KEY}
-api_key_secret=${LOCAL_API_SECRET_KEY}
+source .env
 req_date=$(TZ=UTC date +'%FT%T')
 
 if ! [ -z "${json_data}" ]; then
+    ciphertext=$(echo -ne "${http_method}\n${path_uri}\n${req_date}\n$(echo -n $json_data | openssl enc -A -base64)" | openssl dgst -${digest} -hmac "${LOCAL_API_SECRET_KEY}" | sed 's/^.*= //')
     echo -n ${json_data} | curl -s --compressed \
         --dump-header headers.log \
         -X ${http_method} \
         -H 'Content-Type: application/json' \
         -H "X-Digest: HMAC-$(printf '%s\n' $digest | awk '{ print toupper($0) }')" \
-        -H "X-Signature: $(echo -n "${http_method}\n${path_uri}\n${req_date}\n$(echo -n $json_data | openssl enc -A -base64)" | openssl dgst -${digest} -hmac "${api_key_secret}" | sed 's/^.*= //')" \
-        -H "X-ApiKey: ${api_key}" \
+        -H "X-Signature: ${ciphertext}" \
+        -H "X-ApiKey: ${LOCAL_API_KEY}" \
         -H "X-Date: ${req_date}" \
         --data @- -- ${domain_url}${path_uri}
 else
+    ciphertext=$(echo -ne "${http_method}\n${path_uri}\n${req_date}" | openssl dgst -${digest} -hmac "${LOCAL_API_SECRET_KEY}" | sed 's/^.*= //')
     curl -s --compressed \
         --dump-header headers.log \
         -X ${http_method} \
         -H 'Content-Type: application/json' \
         -H "X-Digest: HMAC-$(printf '%s\n' $digest | awk '{ print toupper($0) }')" \
-        -H "X-Signature: $(echo -n "${http_method}\n${path_uri}\n${req_date}" | openssl dgst -${digest} -hmac "${api_key_secret}" | sed 's/^.*= //')" \
-        -H "X-ApiKey: ${api_key}" \
+        -H "X-Signature: ${ciphertext}" \
+        -H "X-ApiKey: ${LOCAL_API_KEY}" \
         -H "X-Date: ${req_date}" \
         ${domain_url}${path_uri}
 fi
